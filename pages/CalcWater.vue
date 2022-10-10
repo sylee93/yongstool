58d03bb32f8068f8b972af127fd752a712f06e24
sylee <sylee050864@gmail.com>
2022-10-10 22:17:17 +0900

    test

--- pages/CalcWater.vue
+++ pages/CalcWater.vue
@@ -173,31 +173,7 @@ export default {
     }
   },
 
-  head: {
-    title: '물타기 계산기(원,달러) - 국내, 해외 주식 평단가 계산',
-    meta: [
-      {
-        hid: 'description',
-        name: 'description',
-        content: '국내 및 해외 또는 코인 자산등의 물타기계산기 투입금액 및 수량 또는 목표단가등 다양한 방식으로 계산하는 물타기 계산기',
-      },
-      {
-        hid: 'keyword',
-        name: 'keyword',
-        content: '국내주식, 해외주식, 코인, 물타기, 목표단가, 추매, 추가구매, 불타기',
-      },
-      {
-        hid: 'og:title',
-        name: 'og:title',
-        content: '국내 & 미국 주식 물타기 계산기',
-      },
-      {
-        hid: 'og:description',
-        name: 'og:description',
-        content: '추매 수량 및 예상 평균 단가 계산 해보기',
-      },
-    ]
-  },
+ 
 
   methods: {
     // 보유 잔고 계산
